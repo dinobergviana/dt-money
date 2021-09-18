fb092bbf7fde187a0abcb23c5d8cfded32d342be
DinobergueViana <dinobergueviana@gmail.com>
2021-09-18 15:01:00 -0300

    adds global styles to project

---
+++ src/styles/global.ts
@@ -0,0 +1,30 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+
+:root {
+    --background: #f0f2f5;
+    --red: #e52e4d;
+    --green: #33cc95;
+    --blue: #5429cc;
+    --blue-light: #6933ff;
+    --orange: #ff512f;
+    --orange-light: #f09819;
+    --gray-dark: #121214;
+    --gray-light: #202024;
+    --text-title: #363f5f;
+    --text-body: #969cb3;
+    --shape: #ffffff;
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background: var(---background);
+    -webkit-font-smoothing: antialiased;
+  }
+`
