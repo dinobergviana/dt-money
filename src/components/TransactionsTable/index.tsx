3b527c74cc72e744f1a34596208cecba1b925451
DinobergueViana <dinobergueviana@gmail.com>
2021-09-18 16:56:24 -0300

    adds TransactionsTable and its styles to project

---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,34 @@
+import { Container } from "./styles";
+
+export function TransactionsTable() {
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Título</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Site</td>
+            <td className="deposit">$ 500,00</td>
+            <td>Freela</td>
+            <td>10/09/2021</td>
+          </tr>
+
+          <tr>
+            <td>Aluguel</td>
+            <td className="withdraw">-$ 800,00</td>
+            <td>Freela</td>
+            <td>17/09/2021</td>
+          </tr>
+          
+        </tbody>
+      </table>
+    </Container>
+  )
+}
