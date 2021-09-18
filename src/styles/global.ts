7e8b8ec3b53b3e7b0062b941bba53e4799a51344
DinobergueViana <dinobergueviana@gmail.com>
2021-09-18 15:50:36 -0300

    adds Header component and its styles

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -6,15 +6,24 @@ export const GlobalStyle = createGlobalStyle`
   --background: #f0f2f5;
   --red: #e52e4d;
   --green: #33cc95;
+
   --blue: #5429cc;
   --blue-light: #6933ff;
-  --orange: #ff512f;
-  --orange-light: #f09819;
-  --gray-dark: #121214;
-  --gray-light: #202024;
+
   --text-title: #363f5f;
   --text-body: #969cb3;
+  
   --shape: #ffffff;
+}
+
+html {
+  @media (max-width: 1080px) {
+    font-size: 93.75%;
+  }
+
+  @media (max-width: 720px) {
+    font-size: 87.5%;
+  }
 }
 
 body, input, textarea, button {
