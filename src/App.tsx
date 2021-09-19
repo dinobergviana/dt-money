17c2419aa3de2ce945601c9e70d2efe61949f309
DinobergueViana <dinobergueviana@gmail.com>
2021-09-19 00:49:47 -0300

    adds react-modal to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,33 @@
+import { useState } from 'react';
+import Modal from 'react-modal';
+
 import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
-import { GlobalStyle } from './styles/global'
+import { GlobalStyle } from './styles/global';
+
+Modal.setAppElement('#root');
 
 export function App() {
+
+  const [isNewTransactionOpen, setIsNewTransactionOpen] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionOpen(false);
+  }
+
   return (
     <>
-      <Header/>
+      <Header onOpenNewTransactionModal={ handleOpenNewTransactionModal }  />
       <Dashboard/>
+      <Modal 
+        isOpen={ isNewTransactionOpen } 
+        onRequestClose={ handleCloseNewTransactionModal }>
+        <h2>Cadastrar transação</h2>
+      </Modal>
       <GlobalStyle />
     </>
   );
